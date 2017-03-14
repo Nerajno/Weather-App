b779d4039663456fc6103f0b4262ac4f33f74953
Nerajno <Nerajno@gmail.com>
2017-03-13 22:30:22 -0400

    Understanding the API

--- js/WA.js
+++ js/WA.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
+var long;
+var lat;
+//get location code from freeCodecamp = http://bit.ly/2nxT7pG
+  if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(function(position) {
+   long = position.coords.longitude;
+   lat = position.coords.latitude;
+  $("#data").html("latitude: " + lat+ "<br>longitude: " + long);
 
-var api = //"http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=32317440735df175bc0a92379a41137c";
-  
+});
+}
+
+//JSON call for Open weather API
+var api ="http://api.openweathermap.org/data/2.5/weather?q=30349,us&appid=32317440735df175bc0a92379a41137c";
+   $.getJSON(api,function(data){
+
+    //alert(data.coord.lat);
+    console.log(api);
+   });
  });
