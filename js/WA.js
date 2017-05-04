f59b563612589212176cdd91a5f9b35fe277c475
Nerajno <Nerajno@gmail.com>
2017-05-03 23:34:35 -0400

    Issues

--- js/WA.js
+++ js/WA.js
@@ -4,21 +4,28 @@ var lat;
 //get location code from freeCodecamp = http://bit.ly/2nxT7pG
   if(navigator.geolocation) {
   navigator.geolocation.getCurrentPosition(function(position) {
-  var long = position.coords.longitude;
-  var lat = position.coords.latitude;
+    long = position.coords.longitude;
+    lat = position.coords.latitude;
   $("#data").html("latitude:"+lat+"<br>longitude:"+long);
-  //console.log(long);
+  console.log(long,lat);
+
+weather(lat, long);
 
 
 });
 }
 
 //JSON call for Open weather API
-var api ='http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=32317440735df175bc0a92379a41137c';
-   $.getJSON(api,function(data){
-    alert(data.coords.lat);
-    console.log(api);
-   //api.openweathermap.org/data/2.5/weather?lat=35&lon=139
+var weather = function (lat, lon) {
+    var api ='http://api.openweathermap.org/data/2.5/weather?lat=' +lat+'&lon=' +long+'&appid=10ddf1a5e1f2aa9f4b606d5025f56ed5';
+       $.getJSON(api,function(data){
+      //  alert(data.coords.lat);
+        console.log(data);
+       //api.openweathermap.org/data/2.5/weather?lat=35&lon=139
 
- });
+     });
+}
+
+
+
  });
--- js/WA2.js
+++ js/WA2.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
-
   var long;
   var lat;
   var temp;
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function(position){
       long = position.coords.latitude;
       lat = position.coords.longitude;
-
       //Creating a weather API integrating the Open Weather API
-     var api ='https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=10ddf1a5e1f2aa9f4b606d5025f56ed5';
+      //8ffc498530cba7a0
+      var opener = '8ffc498530cba7a0';
+      var api = "http://api.wunderground.com/api/8ffc498530cba7a0/forecast/geolookup/conditions/q/" + lat + "," + long + ".json";
+     //var api ='https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+long+'&appid=10ddf1a5e1f2aa9f4b606d5025f56ed5';
+       console.log(api);
       $.getJSON(api,function(data){
         //JSON call for Open weather API
         var kelvin = data.main.temp;
