28f15c8661c33a667a414688482d79bbabb45e69
github username <github email address>
2020-02-13 22:07:14 -0500

    refactor v2 of js

---
+++ js/weatherApp.js
@@ -0,0 +1,35 @@
+// location
+// time
+// temperature
+// wind speed 
+// humidty
+// pressure 
+// Sun up 
+// sun down
+
+let location = function getLocation() {
+   
+    function success(position) {
+      const latitude  = position.coords.latitude.toFixed(2);
+      const longitude = position.coords.longitude.toFixed(2);
+  
+      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
+    }
+  
+    function error() {
+      status.textContent = 'Unable to retrieve your location';
+    }
+  
+    if (!navigator.geolocation) {
+      status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+      status.textContent = 'Locating…';
+      navigator.geolocation.getCurrentPosition(success, error);
+    }
+  
+  }
+
+  var watchID = navigator.geolocation.watchPosition(function(position) {
+    do_something(position.coords.latitude, position.coords.longitude);
+  });
